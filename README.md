dae03304060390b9fc4230207306bc0ec561fbb2
Miguel Angel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2022-02-28 23:04:03 -0600

    reto 1

--- README.md
+++ README.md
@@ -1,19 +1,59 @@
-# platzi-music
+# Curso profesional de Vue Js
 
-## Project setup
-```
-npm install
-```
+## Introducción
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
+### Bienvenidos al Curso Profesional de Vue JS
 
-### Compiles and minifies for production
-```
-npm run build
-```
+### Introducción a Vue JS
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Vue JS nos permite hacer aplicaciones muy pequeñas y muy grandes.
+
+Vue JS está pensado para manejar distintos tipos de complejidades. Tenemos por ejemplo 2; inherente (la que viene del proyecto y no podemos modificar) e instrumental (la que nosotros podemos controlar).
+
+Estas complejidades tienen que crecer o decrecer a la par. Vue JS es el framework progresivo y nos ayuda con esto.
+
+Vue JS usa rendering declarativo y sistema de componentes.
+
+## CLI y Dev Tools
+
+### Herramientas y Experiencia de Desarrollo + Archivos .vue
+
+Debemos tener instalado Node JS y sirve tener también las Vue Dev Tools en el navegador. Además necesitamos instalar la vue-cli para crear proyectos.
+
+### CLI - Hello World
+
+### Webpack
+
+Webpack es un empaquetador que crea un bundle de todos los archivos que necesitamos para el proyecto.
+
+Hoy en día Webpack corre por detrás de Vue, no es necesario configurarlo.
+
+### Babel
+
+EcmaScript es el standar que usamos para escribir JavaScript moderno.
+
+Babel es un trasnpilador de código JS que convierte nuestro código moderno a código que sea soportado por los navegadores.
+
+Puedes encontrar más información en la [documentación de Babel](https://babeljs.io/)
+
+### ESLint
+
+Este es un linter de código. Nos permite definir reglas para que todos los programadores en el grupo del proyecto escriban código con las mismas reglas.
+
+Puedes encontrar más información en la [documentación de ESLint](https://eslint.org/)
+
+La instalamos con `npm i -D eslint` o también la pudimos haber instalado desde la creación de la aplicación.
+
+[Aquí más información](https://eslint.org/docs/user-guide/getting-started)
+
+### SASS y BULMA
+
+Podemos usar SASS desde el momento en que estamos creando la aplicación con la vue-cli.
+
+Bulma es un framework de CSS. [Documentación oficial](https://bulma.io/documentation/)
+
+Lo instalamos con `npm i bulma`
+
+### PUG
+
+[Documentación oficial](https://pugjs.org/api/getting-started.html)
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,102 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <img src="@/assets/platzi_foto.jpg" />
+    <h1 class="title">{{ msgH1 }}</h1>
+    <form class="form">
+      <label class="nameInput">
+        <small>Nombre</small>
+        <input type="text" name="" id="" placeholder="Ejemplo: Miguel">
+      </label>
+      <label class="lastNameInput">
+        <small>Apellido</small>
+        <input type="text" name="" id="" placeholder="Ejemplo: Reyes">
+      </label>
+      <label class="workInput">
+        <small>Cargo</small>
+        <input type="text" name="" id="" placeholder="Ejemplo: Software Developer">
+      </label>
+      <button class="sendButton" type="submit">
+        ENVIAR
+      </button>
+    </form>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      msgH1: 'Registro estación Platzi',
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+$blue: #00bfd6;
+$blue2: #a3ecf3;
+$orange: #ff9300;
+$black: #212121;
+
+#app, .form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 
-nav {
-  padding: 30px;
+#app {
+  width: 409px;
+  height: 320px;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  & img {
+    width: 100%;
+    height: auto;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
+  & .title {
+    font-style: bold;
+    color: $blue;
+  }
+
+  & .form {
+    width: 100%;
+    & small {
+      color: $orange;
+      font-weight: bold;
+      font-size: 10px;
+    }
+
+    & input, & label {
+      width: 100%;
+    }
+
+    & input {
+      border: none;
+      border-bottom: 2px solid $orange;
+      font-size: 16px;
+      color: $black;
+
+      &:focus {
+        outline: none;
+      }
+    }
+
+    & .sendButton {
+      width: fit-content;
+      margin-top: 20px;
+      align-self: flex-end;
+      background: $blue;
+      color: white;
+      border: none;
+      border-radius: 2px;
+      font-size: 16px;
+      padding: 10px 40px;
+      box-shadow: 0px 5px 5px 0px rgb(0 0 0 / 25%);
+
+      &:hover {
+        background: $blue2;
+      }
     }
   }
 }
