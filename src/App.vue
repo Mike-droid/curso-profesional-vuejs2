fe2fa2aa45036324aa875bec66f934b9a8349040
Miguel Angel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2022-03-13 19:54:05 -0500

    ui de todo list

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,32 @@
     <div class="tareas">
       <input type="text" name="" id="taskTitle" placeholder="Tarea">
       <input type="number" name="" id="taskTime" placeholder="Horas" min="1" step="1">
-      <button @click="addTask">Agregar tarea</button>
+      <button @click="addTask" class="addTask button">
+        <img class="icon" src="./assets/icons/mas.png" alt="Agregar tarea">
+      </button>
     </div>
-    <section v-if="tasks.length > 0">
+
+    <section v-if="tasks.length > 0" class="section_tareas">
       <div class="listaTareas">
         <ul>
           <li
-            v-for="task, index in tasks"
-            :key="index"
+            v-for="(task, key) in tasks"
+            :key="key"
+            ref="task"
           >
             {{ task.title }} -> {{ task.time }} horas
           <button
-            :key="index"
             @click="deleteTask"
+            class="deleteTask button"
           >
-            Borrar tarea {{ task.title }}
+            <img class="icon" src="./assets/icons/borrar.png" alt="Borrar tarea">
           </button>
           </li>
         </ul>
       </div>
 
       <div class="tiempo">
-        <p>Tiempo total: {{ totalTime }} horas</p>
+        <h2>Tiempo total: {{ totalTime }} horas</h2>
       </div>
     </section>
     <section v-else>
@@ -40,7 +44,7 @@ export default {
       tasks: [],
       newTask: {
         title: '',
-        time: 0
+        time: 0,
       }
     };
   },
@@ -55,7 +59,7 @@ export default {
         this.tasks.push(this.newTask);
         this.newTask = {
           title: '',
-          time: 0
+          time: 0,
         };
       } else {
         alert('La tarea y el tiempo deben tener un valor');
@@ -64,11 +68,21 @@ export default {
       localStorage.setItem('tasks', JSON.stringify(this.tasks));
     },
 
-    deleteTask(task) {
-      const index = this.tasks.indexOf(task);
+    deleteTask(taskIndex) {
+      const taskToDelete = taskIndex.path[2]
+      const arrayTasks = Array.from(taskIndex.path[3].children);
+
+      let index
+
+      arrayTasks.forEach((task, key) => {
+        if (task === taskToDelete) {
+          index = key;
+        }
+      });
+
       this.tasks.splice(index, 1);
       localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    }
+    },
   },
 
   computed: {
@@ -91,5 +105,112 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap');
+#app {
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  font-family: 'Inter', sans-serif;
 
+  & .tareas {
+    display: grid;
+    grid-template-areas: "title time"
+                          "button button";
+    justify-items: center;
+    align-self: start;
+    width: 40%;
+    margin-block-start: 20px;
+
+    input {
+      width: 95%;
+      font-family: 'Inter', sans-serif;
+      padding: 5px;
+      font-size: 18px;
+      border-top: none;
+      border-left: none;
+      border-right: none;
+      border-width: 2px;
+      border-color: rgb(151, 147, 147);
+      outline: none;
+    }
+
+    & #taskTitle {
+      grid-area: title;
+    }
+
+    & #taskTime {
+      grid-area: time;
+    }
+
+    & .addTask {
+      grid-area: button;
+      margin-block-start: 20px;
+    }
+  }
+
+  & .section_tareas{
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    width: 40%;
+
+    & .listaTareas {
+      & ul {
+        list-style: none;
+        padding: 0;
+        pointer-events: none;
+
+        & .deleteTask {
+          pointer-events: auto;
+        }
+
+        & li {
+          display: flex;
+          transition: 0.5s;
+          align-items: center;
+          justify-content: space-between;
+          width: 400px;
+          margin-block: 15px;
+          border: 1px solid black;
+          border-radius: 10px;
+          padding: 5px;
+
+          &:hover {
+            background-color: crimson;
+            opacity: 0.9;
+            color: white;
+          }
+        }
+      }
+    }
+  }
+
+  & .icon {
+    width: 25px;
+  }
+
+  & .tiempo {
+    width: 40%;
+    align-self: start;
+    margin-left: 25px;
+
+    h2 {
+      text-align: end;
+    }
+  }
+
+  & .button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: none;
+    border: none;
+    outline: none;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
 </style>
